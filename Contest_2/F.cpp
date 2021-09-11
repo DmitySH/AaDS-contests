90850d71b2dd8efe8949138c14dfd4e7c2262dfa
Дмитрий Шагаров <dashagarov@edu.hse.ru>
2021-09-11 11:46:32 +0300

    Second contest done

--- Contest_2/F.cpp
+++ Contest_2/F.cpp
@@ -1,9 +1,49 @@
 #include <iostream>
+#include <cstring>
+
+using std::cout;
+using std::cin;
+using std::endl;
+
+int calls = 0;
+
+int gcd(int a, int b) {
+    calls++;
+    if (!b) {
+        return a;
+    }
+    else {
+        return gcd(b, a % b);
+    }
+}
 
 int main() {
     std::ios_base::sync_with_stdio(false);
     std::cin.tie(nullptr);
 
+    int n;
+    int cur_gcd = -1;
+    std::string inp;
+    cin >> n;
+
+    for (int i = 0; i < n; ++i) {
+        cin >> inp;
+        if (std::strchr(inp.c_str(), '.')) {
+            cout << "Sequence has float values!";
+            return 0;
+        }
+        else {
+            int num = std::stoi(inp);
+            if(cur_gcd == -1) {
+                cur_gcd = num;
+            }
+            else {
+                cur_gcd = gcd(num, cur_gcd);
+            }
+        }
+    }
+
+    cout << cur_gcd << " " << calls;
 
     return 0;
 }
--- Contest_2/G.cpp
+++ Contest_2/G.cpp
@@ -1,9 +1,48 @@
 #include <iostream>
+#include <cstring>
+
+using std::cout;
+using std::cin;
+using std::endl;
+
+long long gcd(long long a, long long b) {
+    if (!b) {
+        return a;
+    }
+    else {
+        return gcd(b, a % b);
+    }
+}
+
+long long lcm(long long a, long long b) {
+    return a / gcd(a, b) * b ;
+}
 
 int main() {
     std::ios_base::sync_with_stdio(false);
     std::cin.tie(nullptr);
 
+    long long n;
+    long long cur_lcm = -1;
+    std::string inp;
+    cin >> n;
+
+    for (int i = 0; i < n; ++i) {
+        cin >> inp;
+        if (std::strchr(inp.c_str(), '.')) {
+            cout << "Sequence has float values!";
+            return 0;
+        } else {
+            int num = std::stoi(inp);
+            if (cur_lcm == -1) {
+                cur_lcm = num;
+            } else {
+                cur_lcm = lcm(num, cur_lcm);
+            }
+        }
+    }
+
+    cout << std::abs(cur_lcm);
 
     return 0;
 }

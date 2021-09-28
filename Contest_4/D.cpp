3fd6b92f9a2b8dfd5603dd08af89c323569d685b
Дмитрий Шагаров <dashagarov@edu.hse.ru>
2021-09-28 18:56:26 +0300

    D added

--- Contest_4/D.cpp
+++ Contest_4/D.cpp
@@ -1,16 +1,48 @@
 #include <iostream>
 #include <string>
 
-int pow(int num, int pow) {
-    if (pow == 0) {
-        return 1;
-    }
-    for (int i = 1; i < pow; ++i) {
-        num *= num;
+struct Timer {
+ private:
+    int64_t days, hours, minutes, seconds;
+
+ public :
+
+    explicit Timer(int64_t seconds) {
+        days = seconds / 3600 / 24;
+        hours = seconds / 3600 % 24;
+        minutes = seconds % 3600 / 60;
+        this->seconds = seconds % 3600 % 60;
     }
 
-    return num;
-}
+    void AddHours(int64_t additional_hours) {
+        days += (additional_hours + hours) / 24;
+        hours = (hours + additional_hours) % 24;
+    }
+
+    void AddMinutes(int64_t additional_minutes) {
+        AddHours((additional_minutes + minutes) / 60);
+        minutes = (minutes + additional_minutes) % 60;
+    }
+
+    void AddSeconds(int64_t additional_seconds) {
+        AddMinutes((additional_seconds + seconds) / 60);
+        seconds = (seconds + additional_seconds) % 60;
+    }
+
+    std::string ToStringTime() const {
+        std::string result = std::to_string(hours / 10) + std::to_string(hours % 10)
+                             + ':' + std::to_string(minutes / 10)
+                             + std::to_string(minutes % 10) + ':'
+                             + std::to_string(seconds / 10) +
+                             std::to_string(seconds % 10);
+
+        if (days != 0) {
+            result += '+' + std::to_string(days) + " days";
+        }
+
+        return result;
+    }
+};
 
 std::string *SplitString(const std::string &str, char sym, int n) {
     int pos = str.find(sym);
@@ -19,7 +51,6 @@ std::string *SplitString(const std::string &str, char sym, int n) {
 
     for (int i = 0; i < n - 1; ++i) {
         pos = str.find(sym, prev_pos);
-
         a[i] = str.substr(prev_pos, pos - prev_pos);
         prev_pos = pos + 1;
     }
@@ -31,7 +62,7 @@ std::string *SplitString(const std::string &str, char sym, int n) {
 int CountSymbols(const std::string &str, char sym) {
     int syms_count = 0;
     int pos = str.find(sym);
-    int prev_pos = 0;
+    int prev_pos;
     while (pos != -1) {
         ++syms_count;
         prev_pos = pos + 1;
@@ -41,40 +72,35 @@ int CountSymbols(const std::string &str, char sym) {
     return syms_count;
 }
 
-std::string Timer(const std::string &input_first, const std::string &input_second) {
-    int total_seconds = ((input_first[0] - '0') * 10 + (input_first[1] - '0')) * 3600 +
-                          ((input_first[3] - '0') * 10 + (input_first[4] - '0')) * 60 +
-                        ((input_first[6] - '0') * 10 + (input_first[7] - '0'));
+std::string CountTimer(const std::string &input_first, const std::string &input_second) {
+    int64_t total_seconds = ((input_first[0] - '0') * 10 + (input_first[1] - '0')) * 3600 +
+                            ((input_first[3] - '0') * 10 + (input_first[4] - '0')) * 60 +
+                            ((input_first[6] - '0') * 10 + (input_first[7] - '0'));
+
+    Timer timer(total_seconds);
 
     int number_of_nums = CountSymbols(input_second, ':') + 1;
     std::string *a = SplitString(input_second, ':', number_of_nums);
 
-    int pow_number = 0;
-    for (int i = number_of_nums - 1; i >= 0; --i) {
-        total_seconds += std::stoi(a[i]) * pow(60, pow_number);
-        ++pow_number;
+    switch (number_of_nums) {
+        case 1:
+            timer.AddSeconds(std::stoll(a[0]));
+            break;
+        case 2:
+            timer.AddSeconds(std::stoll(a[1]));
+            timer.AddMinutes(std::stoll(a[0]));
+            break;
+        case 3:
+            timer.AddSeconds(std::stoll(a[2]));
+            timer.AddMinutes(std::stoll(a[1]));
+            timer.AddHours(std::stoll(a[0]));
+            break;
+        default:
+            return "";
     }
 
-    int new_seconds, new_minutes, new_hours, days;
-
-    days = total_seconds / 3600 / 24;
-    new_hours = total_seconds / 3600 % 24;
-    new_minutes = total_seconds % 3600 / 60;
-    new_seconds = total_seconds % 3600 % 60;
-
-    std::string result = std::to_string(new_hours / 10) + std::to_string(new_hours % 10)
-                         + ':' + std::to_string(new_minutes / 10)
-                         + std::to_string(new_minutes % 10) + ':'
-                         + std::to_string(new_seconds / 10) +
-                         std::to_string(new_seconds % 10);
-
-    if (days != 0) {
-        result += '+' + std::to_string(days) + " days";
-    }
-
-    return result;
+    return timer.ToStringTime();
 }
-
 
 int main() {
     std::ios_base::sync_with_stdio(false);
@@ -83,7 +109,7 @@ int main() {
     std::string input_first, input_second;
     std::cin >> input_first >> input_second;
 
-    std::cout << Timer(input_first, input_second);
+    std::cout << CountTimer(input_first, input_second);
 
     return 0;
 }

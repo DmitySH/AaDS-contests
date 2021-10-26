6c9281f05c820b2c9db1f73d59254b413a61aaac
Дмитрий Шагаров <dashagarov@edu.hse.ru>
2021-10-27 02:34:39 +0300

    Not my checks

--- Contest_6/Eitmo.cpp
+++ Contest_6/Eitmo.cpp
@@ -1,131 +1,101 @@
-// Prod. by Andryuschenko Michael in 2021.
-
 #include <iostream>
-#include <string>
-// Used only by sqrt.
-#include <cmath>
 
-template <typename T>
-using Operation = T (*)(T first_element, T second_element);
-
-template <typename T>
-class SpecifyList {
-public:
-    void build(T *array, int size) {
-        //        if (!isFirst) {
-        //            clear();
-        //        }
-        //        isFirst = false;
-        clear();
-
-        size_ = size;
-        block_length_ = static_cast<int>(std::sqrt(size)) + 1;
-        count_block_ = (size + block_length_ - 1) / block_length_;
-        array_ = new T *[count_block_];
-        size_of_arrays_ = new T[count_block_];
-
-        for (int i = 0; i < count_block_; ++i) {
-            array_[i] = new T[block_length_];
-            size_of_arrays_[i] = block_length_;
-        }
-
-        size_of_arrays_[count_block_ - 1] = size % block_length_;
-
-        for (int i = 0; i < size; ++i) {
-            array_[i / block_length_][i % block_length_] = array[i];
-        }
-    }
-
-    void readData(int count_elements) {
-        T *array = new T[count_elements];
-        for (int i = 0; i < count_elements; ++i) {
-            std::cin >> array[i];
-        }
-
-        build(array, count_elements);
-
-        delete[] array;
-    }
-
-    void setOperation(char operation) {
-        if (operation == '+') {
-            operation_ = [](T first, T second) { return first + second; };
-        }
-
-        if (operation == '^') {
-            operation_ = [](T first, T second) { return first ^ second; };
-        }
-    }
-
-    SpecifyList() {
-        array_ = new T *[2];
-        size_of_arrays_ = new T[2];
-    }
-
-    ~SpecifyList() {
-        for (int i = 0; i < count_block_; ++i) {
-            delete[] array_[i];
-        }
-
-        delete[] array_;
-        delete[] size_of_arrays_;
-        array_ = nullptr;
-        size_of_arrays_ = nullptr;
-    }
-
+class MyBinaryTree {
 private:
-    bool isFirst = true;
-    int size_ = 0;
-    int block_length_ = 0;
-    int count_block_ = 0;
-    T **array_;
-    int *size_of_arrays_;
-    int count_operation_ = 0;
-    Operation<T> operation_;
+    struct TreeNode {
+        int data;
+        TreeNode *left;
+        TreeNode *right;
+    };
 
-    void clear() {
-        for (int i = 0; i < count_block_; ++i) {
-            delete[] array_[i];
-        }
+    TreeNode *head_;
 
-        delete[] array_;
-        delete[] size_of_arrays_;
-
-        size_ = 0;
-        block_length_ = 0;
-        count_block_ = 0;
-        count_operation_ = 0;
-        array_ = new T *[count_block_];
-        size_of_arrays_ = new T[block_length_];
-    }
-
-    void rebuild() {
-        T *array = new T[size_];
-        int current_position = 0;
-
-        for (int i = 0; i < count_block_; ++i) {
-            for (int j = 0; j < size_of_arrays_[i]; ++j) {
-                array[current_position] = array_[i][j];
+    void addNode(TreeNode *node, int data) {
+        if (node->data > data) {
+            if (node->left == nullptr) {
+                auto *nd = new TreeNode;
+                nd->data = data;
+                nd->left = nullptr;
+                nd->right = nullptr;
+                node->left = nd;
+            } else {
+                addNode(node->left, data);
+            }
+        } else {
+            if (node->right == nullptr) {
+                auto *nd = new TreeNode;
+                nd->data = data;
+                nd->left = nullptr;
+                nd->right = nullptr;
+                node->right = nd;
+            } else {
+                addNode(node->right, data);
             }
         }
+    }
 
-        build(array, size_);
+    int getHeight(TreeNode *node) {
+        if (node == nullptr) {
+            return -1;
+        }
+        int left = getHeight(node->left);
+        int right = getHeight(node->left);
+
+        if (left > right) {
+            return left + 1;
+        } else {
+            return right + 1;
+        }
+    }
+
+    void clear(TreeNode *node) {
+        if (node != nullptr) {
+            clear(node->left);
+            clear(node->right);
+            delete node;
+        }
+    }
+
+
+
+public:
+    explicit MyBinaryTree(int data) {
+        auto *node = new TreeNode;
+        node->data = data;
+        node->left = nullptr;
+        node->right = nullptr;
+        head_ = node;
+    }
+
+    ~MyBinaryTree() {
+        clear(head_);
+    }
+
+    void addNode(int data) {
+        addNode(head_, data);
+    }
+
+    int getHeight() {
+        return getHeight(head_);
     }
 };
 
 int main() {
-    SpecifyList<int> ans;
-    int *kek = new int[5];
-    kek[0] = 1;
-    kek[1] = 2;
-    kek[2] = 3;
-    kek[3] = 4;
-    kek[4] = 5;
-
-    ans.build(kek, 5);
-
-    delete[] kek;
-
-    std::cout << "1";
+    int num;
+    std::cin >> num;
+    if (num == 0) {
+        std::cout << 0;
+        return 0;
+    }
+    auto *tree = new MyBinaryTree(num);
+    while (true) {
+        std::cin >> num;
+        if (num == 0) {
+            break;
+        }
+        tree->addNode(num);
+    }
+    std::cout << tree->getHeight() + 1;
+    delete tree;
     return 0;
 }

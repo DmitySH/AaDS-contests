d52e92484b23b1cb1e50bd5f7f3a8f148cc94d14
dmitry <di.im2015@yandex.ru>
2021-11-11 16:41:04 +0300

    C done

--- Contest_9/C.cpp
+++ Contest_9/C.cpp
@@ -1,3 +1,5 @@
+#include <iostream>
+
 struct Node {
 public:
     int key;
@@ -10,8 +12,13 @@ public:
         delete left;
     }
 
-private:
-    // your code
+    bool leftChild() {
+        return this == this->parent->left;
+    }
+
+    bool rightChild() {
+        return this == this->parent->right;
+    }
 };
 
 class SplayTree {
@@ -61,28 +68,101 @@ public:
                 return cur;
             }
         }
+
         return nullptr;
     }
 
-    int splay(Node *) {
+    int splay(Node *node) {
+        int rotates = 0;
+        while (node != root_) {
+            Node *parent = node->parent;
+
+            if (!parent->parent) {
+                ++rotates;
+                if (node->leftChild()) {
+                    zig(parent);
+                } else {
+                    zag(parent);
+                }
+            } else {
+                ++rotates;
+                if (node->leftChild() && parent->leftChild()) {
+                    zig(parent->parent);
+                    zig(parent);
+                    ++rotates;
+                } else if (node->rightChild() && parent->rightChild()) {
+                    zag(parent->parent);
+                    zag(parent);
+                    ++rotates;
+                } else if (node->rightChild() && parent->leftChild()) {
+                    zag(parent);
+                    zig(node->parent);
+                } else {
+                    zig(parent);
+                    zag(node->parent);
+                }
+            }
+        }
+
+        return rotates;
     }
 
     int getHeight() const {
+        int depth = traverse(root_) - 1;
+        return depth < 0 ? 0 : depth;
     }
 
 private:
     Node *root_;
+
+    int traverse(Node *cur) const {
+        int left;
+        int right;
+        int height = 0;
+
+        if (cur) {
+            left = traverse(cur->left);
+            right = traverse(cur->right);
+            height = ((left > right) ? left : right) + 1;
+        }
+
+        return height;
+    }
+
+    void zag(Node *pivot) {
+        Node *temp = pivot->right;
+        pivot->right = temp->left;
+
+        if (temp->left) {
+            temp->left->parent = pivot;
+        }
+
+        change(pivot, temp);
+        temp->left = pivot;
+        pivot->parent = temp;
+    }
+
+    void zig(Node *pivot) {
+        Node *temp = pivot->left;
+        pivot->left = temp->right;
+
+        if (temp->right) {
+            temp->right->parent = pivot;
+        }
+
+        change(pivot, temp);
+        temp->right = pivot;
+        pivot->parent = temp;
+    }
+
+    void change(Node *first, Node *second) {
+        if (!first->parent) {
+            root_ = second;
+        } else if (first == first->parent->left) {
+            first->parent->left = second;
+        } else {
+            first->parent->right = second;
+        }
+        second->parent = first->parent;
+    }
 };
-//
-// int main() {
-//    SplayTree st;
-//    st.insert(1);
-//    st.insert(3);
-//    st.insert(2);
-//    st.insert(7);
-//    st.insert(3);
-//    st.insert(6);
-//
-//    int k = 5;
-//    return 0;
-//}
